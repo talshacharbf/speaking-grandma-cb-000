33911a311ee19a45fa0c391d88f3881b09f17354
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 06:15:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase = uppercase.phrase
+  elsif phrase == uppercase.phrase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
+  end
 end
