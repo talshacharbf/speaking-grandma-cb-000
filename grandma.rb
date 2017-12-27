83f45daefdd28641b1156691862bed923e1087b8
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 06:10:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -17,8 +17,4 @@ def speak_to_grandma(phrase)
   elsif phrase = uppercase.phrase
     puts NO, NOT SINCE 1938!
   else HUH?! SPEAK UP, SONNY!
-
-    
-    
-    
-    
+end
